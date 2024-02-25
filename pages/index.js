1e1252c2fb5be2b1acf0132a169ff3100c81b2f1
aifu <huzaifabilal226@gmail.com>
2024-02-25 11:54:39 +0500

    added ctf project

--- pages/index.js
+++ pages/index.js
@@ -171,8 +171,8 @@ export default function Home() {
             <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
               <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 className={`${darkMode ? 'text-black' : 'text-white'} text-5xl font-bold`}>Full-Stack Developer</h1>
-                <h2 className={`${classes.h2} text-white`}>Master in&nbsp;<span className={classes.typewriter}></span></h2>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">Welcome to my world of coding and creativity, where Next.js empowers me to bring imagination to life.</p>
+                {/* <h2 className={`${classes.h2} text-white`}>Master in&nbsp;<span className={classes.typewriter}></span></h2> */}
+                <p class="max-w-2xl mt-2 mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">Welcome to my world of coding and creativity, where Next.js empowers me to bring imagination to life.</p>
                 <a href="https://drive.google.com/uc?export=download&id=12yEC_N2mPjo7DtXJ_hUHzaYLvPIQcTR1" class={`inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ${darkMode ? 'focus:ring-primary-900' : ''}`}>                  Download Resume
                   <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10 13l-5-4h3V4h4v5h3l-5 4zm0 2a1 1 0 100 2 1 1 0 000-2z" />
@@ -287,7 +287,7 @@ export default function Home() {
                     <p class={`text-md font-normal ${darkMode ? 'text-gray-600' : 'text-gray-300'} `}>
                       The MERN-based veteran app is a feature-rich web application designed to serve and support veterans.
                     </p>
-                    <a href="https://github.com/huzaifa-bilal-01/Web-Development-MERN-Course-Project-2022.git" title=""
+                    <a href="https://github.com/huzaifa-bilal-01/MERN-Project-VeteranApp" title=""
                       class={`text-white inline-flex items-center px-4 py-2 text-sm font-medium rounded-md ${darkMode ? 'bg-primary-700 hover:bg-primary-800' : 'bg-primary-600 hover:bg-primary-700'}   `}>
                       Go to Github
                       <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -309,7 +309,30 @@ export default function Home() {
                     <p class={`text-md font-normal ${darkMode ? 'text-gray-600' : 'text-gray-300'} `}>
                     Developed a robust REST API Blog Aggregator using the Go programming language which serves as a centralized platform for fetching and aggregating blogs from various sources.
                     </p>
-                    <a href="https://github.com/huzaifa-bilal-01/Web-Development-MERN-Course-Project-2022.git" title=""
+                    <a href="https://github.com/huzaifa-bilal-01/REST-API_BLOG-AGGREGATOR_GOLANG" title=""
+                      class={`text-white inline-flex items-center px-4 py-2 text-sm font-medium rounded-md ${darkMode ? 'bg-primary-700 hover:bg-primary-800' : 'bg-primary-600 hover:bg-primary-700'}   `}>
+                      Go to Github
+                      <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                        fill="currentColor">
+                        <path fill-rule="evenodd"
+                          d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                          clip-rule="evenodd" />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+
+
+                <div class={`rounded-lg overflow-hidden shadow-md ${darkMode ? 'bg-white' : ' bg-gray-800'}`} data-aos="fade-up">
+                  <Image src="/images/ctf.png" width={330} height={330} alt="vet" class="w-full h-44 object-cover" />
+                  <div class="p-4 space-y-2">
+                    <h3 class="text-xl font-medium text-slate-500">
+                      CTF Challenger Platform
+                    </h3>
+                    <p class={`text-md font-normal ${darkMode ? 'text-gray-600' : 'text-gray-300'} `}>
+                    Developed a Web platform used to host Capture The Flag competitions.
+                    </p>
+                    <a href="https://github.com/mehmoodharis74/Ctf-Challenger-Platform" title=""
                       class={`text-white inline-flex items-center px-4 py-2 text-sm font-medium rounded-md ${darkMode ? 'bg-primary-700 hover:bg-primary-800' : 'bg-primary-600 hover:bg-primary-700'}   `}>
                       Go to Github
                       <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -445,12 +468,12 @@ export default function Home() {
                 <span class="text-sm text-gray-500 sm:text-center">© 2023 <a href="https://flowbite.com" class="hover:underline">AIFU Portfolio™</a>. All Rights Reserved.
                 </span>
                 <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-                  <a href="https://www.facebook.com/profile.php?id=100093702731832" class={`text-gray-500 ${darkMode ? 'hover:text-gray-900' : 'hover:text-white'}  `}>
+                  {/* <a href="https://www.facebook.com/profile.php?id=100093702731832" class={`text-gray-500 ${darkMode ? 'hover:text-gray-900' : 'hover:text-white'}  `}>
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
                   </a>
                   <a href="https://www.instagram.com/zaifuu_/" class={`text-gray-500 ${darkMode ? 'hover:text-gray-900' : 'hover:text-white'}  `}>
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" /></svg>
-                  </a>
+                  </a> */}
                   <a href="https://x.com/anime_gotit?t=OzSFvi9dcFFvdVjVVPBTTg&s=09" class={`text-gray-500 ${darkMode ? 'hover:text-gray-900' : 'hover:text-white'}  `}>
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                   </a>
